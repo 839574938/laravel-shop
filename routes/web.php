379d6ddbff5cddb407fa6083fb0fab9947019843
cyc <839574938@qq.com>
2019-03-05 04:34:59 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-
 Route::get('/', 'PagesController@root')->name('root');
 
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes(['verify' => true]);

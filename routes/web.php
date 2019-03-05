2fbd1bd9cfbcbfcbb8e2ecb903c79b6ccbdd39ef
cyc <839574938@qq.com>
2019-03-05 04:55:05 +0000

    收获地址列表

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes(['verify' => true]);
+
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});

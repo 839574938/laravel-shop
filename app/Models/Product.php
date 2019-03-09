2ec46d43657896c323ff283fdb55a8ac8b06a668
cfx <1569687089@qq.com>
2019-03-09 02:35:32 +0000

    第四章

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price'
+        'rating', 'sold_count', 'review_count', 'price','type',
     ];
     protected $casts = [
         'on_sale' => 'boolean',
@@ -26,5 +32,14 @@ class Product extends Model
             return $this->attributes['image'];
         }
         return \Storage::disk('public')->url($this->attributes['image']);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,8 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(CartItem::class);
     }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
